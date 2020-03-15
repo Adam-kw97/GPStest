6a10ecc19eb3b285b3ab8f97b4a4d91bb933449d
ADKW97 <adam.kwidzinski97@gmail.com>
2020-03-15 22:01:57 +0100

    testt wysylania

---
+++ app/src/main/java/com/example/gps/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.gps;
+
+
+//import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+    Point point=new Point(0,0);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse() {
+
+        String url = "https://api.myjson.com/bins/1fjcca";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("coordinates");
+
+                            for (int i = 0; i < 2; i++) {
+                                JSONObject Coordinates = jsonArray.getJSONObject(jsonArray.length()-1-i);
+
+                                point.x = Coordinates.getDouble("longitude");
+                                point.y = Coordinates.getDouble("latidude");
+
+
+                                mTextViewResult.append("X: " + point.x + "\n" + "Y: "+ point.y + "\n\n");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/gps/Point.java
@@ -0,0 +1,13 @@
+package com.example.gps;
+
+public class Point {
+    double x;
+    double y;
+
+
+    public Point(double a, double b){
+        x=a;
+        y=b;
+    }
+    public Point(){}
+}
